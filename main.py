880327f6f75e5511766456008f6d203b94fe26d1
Fasteru <70799803+Fasteru@users.noreply.github.com>
2021-04-12 08:22:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '400')
+
+
+class CalcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "ERROR"
+
+
+class CalculatorApp(App):
+
+    def build(self):
+        return CalcGridLayout()
+
+
+if __name__ == '__main__':
+    CalculatorApp().run()
